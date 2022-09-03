9b3112cf36c46ed14bd51e59d6d9d927ebeaa030
admal <ad.malkova73@gmail.com>
2022-09-03 15:51:27 +0400

    TextBox addition & design fixes

--- UserControlsTest/Form1.Designer.cs
+++ UserControlsTest/Form1.Designer.cs
@@ -30,18 +30,27 @@ namespace UserControlsTest
         private void InitializeComponent()
         {
             this.buttonClearComboBox = new System.Windows.Forms.Button();
-            this.userControlListBox = new VisualComponents.UserControlListBox();
-            this.userControlComboBox = new VisualComponents.UserControlComboBox();
             this.buttonGetObject = new System.Windows.Forms.Button();
             this.labelObjectInfo = new System.Windows.Forms.Label();
             this.labelComboBox = new System.Windows.Forms.Label();
             this.buttonGetFromComboBox = new System.Windows.Forms.Button();
             this.labelEvents = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.buttonGetFromTextBox = new System.Windows.Forms.Button();
+            this.labelForTextBox = new System.Windows.Forms.Label();
+            this.userControlListBox = new VisualComponents.UserControlListBox();
+            this.userControlTextBox = new VisualComponents.UserControlTextBox();
+            this.userControlComboBox1 = new VisualComponents.UserControlComboBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonClearComboBox
             // 
-            this.buttonClearComboBox.Location = new System.Drawing.Point(392, 113);
+            this.buttonClearComboBox.Location = new System.Drawing.Point(44, 90);
             this.buttonClearComboBox.Name = "buttonClearComboBox";
             this.buttonClearComboBox.Size = new System.Drawing.Size(103, 23);
             this.buttonClearComboBox.TabIndex = 2;
@@ -49,25 +58,9 @@ namespace UserControlsTest
             this.buttonClearComboBox.UseVisualStyleBackColor = true;
             this.buttonClearComboBox.Click += new System.EventHandler(this.buttonClearComboBox_Click);
             // 
-            // userControlListBox
-            // 
-            this.userControlListBox.Location = new System.Drawing.Point(3, 2);
-            this.userControlListBox.Name = "userControlListBox";
-            this.userControlListBox.SelectedRowIndex = -1;
-            this.userControlListBox.Size = new System.Drawing.Size(287, 180);
-            this.userControlListBox.TabIndex = 1;
-            // 
-            // userControlComboBox
-            // 
-            this.userControlComboBox.Location = new System.Drawing.Point(255, -25);
-            this.userControlComboBox.Name = "userControlComboBox";
-            this.userControlComboBox.SelectedValue = "";
-            this.userControlComboBox.Size = new System.Drawing.Size(408, 141);
-            this.userControlComboBox.TabIndex = 0;
-            // 
             // buttonGetObject
             // 
-            this.buttonGetObject.Location = new System.Drawing.Point(85, 188);
+            this.buttonGetObject.Location = new System.Drawing.Point(94, 278);
             this.buttonGetObject.Name = "buttonGetObject";
             this.buttonGetObject.Size = new System.Drawing.Size(94, 23);
             this.buttonGetObject.TabIndex = 3;
@@ -78,7 +71,7 @@ namespace UserControlsTest
             // labelObjectInfo
             // 
             this.labelObjectInfo.AutoSize = true;
-            this.labelObjectInfo.Location = new System.Drawing.Point(29, 254);
+            this.labelObjectInfo.Location = new System.Drawing.Point(91, 311);
             this.labelObjectInfo.Name = "labelObjectInfo";
             this.labelObjectInfo.Size = new System.Drawing.Size(46, 17);
             this.labelObjectInfo.TabIndex = 4;
@@ -87,7 +80,7 @@ namespace UserControlsTest
             // labelComboBox
             // 
             this.labelComboBox.AutoSize = true;
-            this.labelComboBox.Location = new System.Drawing.Point(682, 71);
+            this.labelComboBox.Location = new System.Drawing.Point(176, 128);
             this.labelComboBox.Name = "labelComboBox";
             this.labelComboBox.Size = new System.Drawing.Size(46, 17);
             this.labelComboBox.TabIndex = 6;
@@ -95,7 +88,7 @@ namespace UserControlsTest
             // 
             // buttonGetFromComboBox
             // 
-            this.buttonGetFromComboBox.Location = new System.Drawing.Point(532, 68);
+            this.buttonGetFromComboBox.Location = new System.Drawing.Point(179, 90);
             this.buttonGetFromComboBox.Name = "buttonGetFromComboBox";
             this.buttonGetFromComboBox.Size = new System.Drawing.Size(94, 23);
             this.buttonGetFromComboBox.TabIndex = 5;
@@ -106,35 +99,111 @@ namespace UserControlsTest
             // labelEvents
             // 
             this.labelEvents.AutoSize = true;
-            this.labelEvents.Location = new System.Drawing.Point(700, 203);
+            this.labelEvents.Location = new System.Drawing.Point(328, 11);
             this.labelEvents.Name = "labelEvents";
             this.labelEvents.Size = new System.Drawing.Size(16, 17);
             this.labelEvents.TabIndex = 7;
             this.labelEvents.Text = "0";
             // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelObjectInfo);
+            this.panel1.Controls.Add(this.userControlListBox);
+            this.panel1.Location = new System.Drawing.Point(-1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 448);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.labelComboBox);
+            this.panel2.Controls.Add(this.labelEvents);
+            this.panel2.Controls.Add(this.buttonClearComboBox);
+            this.panel2.Controls.Add(this.buttonGetFromComboBox);
+            this.panel2.Controls.Add(this.userControlComboBox1);
+            this.panel2.Location = new System.Drawing.Point(427, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(361, 163);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.labelForTextBox);
+            this.panel3.Controls.Add(this.buttonGetFromTextBox);
+            this.panel3.Controls.Add(this.userControlTextBox);
+            this.panel3.Location = new System.Drawing.Point(427, 170);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(361, 279);
+            this.panel3.TabIndex = 11;
+            // 
+            // buttonGetFromTextBox
+            // 
+            this.buttonGetFromTextBox.Location = new System.Drawing.Point(63, 136);
+            this.buttonGetFromTextBox.Name = "buttonGetFromTextBox";
+            this.buttonGetFromTextBox.Size = new System.Drawing.Size(94, 23);
+            this.buttonGetFromTextBox.TabIndex = 8;
+            this.buttonGetFromTextBox.Text = "Извлечь объект";
+            this.buttonGetFromTextBox.UseVisualStyleBackColor = true;
+            this.buttonGetFromTextBox.Click += new System.EventHandler(this.buttonGetFromTextBox_Click);
+            // 
+            // labelForTextBox
+            // 
+            this.labelForTextBox.AutoSize = true;
+            this.labelForTextBox.Location = new System.Drawing.Point(197, 139);
+            this.labelForTextBox.Name = "labelForTextBox";
+            this.labelForTextBox.Size = new System.Drawing.Size(46, 17);
+            this.labelForTextBox.TabIndex = 8;
+            this.labelForTextBox.Text = "label1";
+            // 
+            // userControlListBox
+            // 
+            this.userControlListBox.Location = new System.Drawing.Point(24, 45);
+            this.userControlListBox.Name = "userControlListBox";
+            this.userControlListBox.SelectedRowIndex = -1;
+            this.userControlListBox.Size = new System.Drawing.Size(373, 225);
+            this.userControlListBox.TabIndex = 1;
+            // 
+            // userControlTextBox
+            // 
+            this.userControlTextBox.Location = new System.Drawing.Point(54, 71);
+            this.userControlTextBox.Name = "userControlTextBox";
+            this.userControlTextBox.Pattern = null;
+            this.userControlTextBox.Size = new System.Drawing.Size(219, 59);
+            this.userControlTextBox.TabIndex = 8;
+            // 
+            // userControlComboBox1
+            // 
+            this.userControlComboBox1.Location = new System.Drawing.Point(44, 11);
+            this.userControlComboBox1.Name = "userControlComboBox1";
+            this.userControlComboBox1.SelectedValue = "";
+            this.userControlComboBox1.Size = new System.Drawing.Size(154, 53);
+            this.userControlComboBox1.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labelEvents);
-            this.Controls.Add(this.labelComboBox);
-            this.Controls.Add(this.buttonGetFromComboBox);
-            this.Controls.Add(this.labelObjectInfo);
+            this.ClientSize = new System.Drawing.Size(791, 450);
             this.Controls.Add(this.buttonGetObject);
-            this.Controls.Add(this.buttonClearComboBox);
-            this.Controls.Add(this.userControlListBox);
-            this.Controls.Add(this.userControlComboBox);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private VisualComponents.UserControlComboBox userControlComboBox;
         private VisualComponents.UserControlListBox userControlListBox;
         private System.Windows.Forms.Button buttonClearComboBox;
         private System.Windows.Forms.Button buttonGetObject;
@@ -142,6 +211,13 @@ namespace UserControlsTest
         private System.Windows.Forms.Label labelComboBox;
         private System.Windows.Forms.Button buttonGetFromComboBox;
         private System.Windows.Forms.Label labelEvents;
+        private VisualComponents.UserControlTextBox userControlTextBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private VisualComponents.UserControlComboBox userControlComboBox1;
+        private System.Windows.Forms.Label labelForTextBox;
+        private System.Windows.Forms.Button buttonGetFromTextBox;
     }
 }
 
--- UserControlsTest/Form1.cs
+++ UserControlsTest/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,10 +16,13 @@ namespace UserControlsTest
         public Form1()
         {
             InitializeComponent();
-            userControlComboBox.InsertData(new List<string>() { "Значение 1", "Значение 2", "Значение 3", "Значение 4", "Значение 5" });
+            userControlComboBox1.InsertData(new List<string>() { "Значение 1", "Значение 2", "Значение 3", "Значение 4", "Значение 5" });
             userControlListBox.SetVisualLayout("Машина марки {Brand}, {ProductionYear} года выпуска", '{', '}');
             userControlListBox.SetListData(new List<Car>() { new Car("Lada", 2018), new Car("Volswagen", 2010), new Car("Volvo", 2020) });
-            userControlComboBox.SetChangeItemEvent += incrementLabelEvents;
+            userControlComboBox1.SetChangeItemEvent += incrementLabelEvents;
+            userControlTextBox.SetToolTip("example@gmail.com");
+            userControlTextBox.SetChangeValueEvent += incrementLabelEvents;
+            userControlTextBox.Pattern = new Regex(@"^([\w\.\-]+)@gmail.com");
         }
         private void incrementLabelEvents(object sender, EventArgs e)
         {
@@ -27,7 +31,7 @@ namespace UserControlsTest
 
         private void buttonClearComboBox_Click(object sender, EventArgs e)
         {
-            userControlComboBox.ClearList();
+            userControlComboBox1.ClearList();
         }
 
         private void buttonGetObject_Click(object sender, EventArgs e)
@@ -37,7 +41,19 @@ namespace UserControlsTest
 
         private void buttonGetFromComboBox_Click(object sender, EventArgs e)
         {
-            labelComboBox.Text = userControlComboBox.SelectedValue;
+            labelComboBox.Text = userControlComboBox1.SelectedValue;
+        }
+
+        private void buttonGetFromTextBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                labelForTextBox.Text = userControlTextBox.Content;
+            }
+            catch(Exception ex)
+            {
+                labelForTextBox.Text = ex.Message;
+            }
         }
     }
 }
